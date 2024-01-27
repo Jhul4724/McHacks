cbb191c1efd88f593616117b9ab96425b0790de6
Hayden <haydenc9898@gmail.com>
2024-01-27 16:34:47 -0500

    html to python inputs working

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import requests
-import json
+# import requests
+# import json
 
-# API endpoint - this might need to be modified based on what data you need
-url = "https://clinicaltrials.gov/api/v2/studies"
+# # API endpoint - this might need to be modified based on what data you need
+# url = "https://clinicaltrials.gov/api/v2/studies"
 
-# Make a GET request
-response = requests.get(url)
+# # Make a GET request
+# response = requests.get(url)
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Convert the response to JSON format
-    data = response.json()
-    # Do something with the data
-    print(data)
-else:
-    print("Failed to retrieve data: Status code", response.status_code)
+# # Check if the request was successful
+# if response.status_code == 200:
+#     # Convert the response to JSON format
+#     data = response.json()
+#     # Do something with the data
+#     print(data)
+# else:
+#     print("Failed to retrieve data: Status code", response.status_code)
