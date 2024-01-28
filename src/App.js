8963f739ddb01b722d6f4323c563445078c5d7f4
Hayden <haydenc9898@gmail.com>
2024-01-27 19:36:31 -0500

    React Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from './Navbar';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [message, setMessage] = useState('bye')
+
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+    .then(res => setMessage(res.text()))
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Navbar ></Navbar>
+          <form action="{{ url_for('info')}}" method="post">
+        <label for="firstname">First Name:</label>
+        <input type="text" id="first name" name="fname" placeholder="first name" />
+        <label for="lastname">Last Name:</label>
+        <input type="text" id="last name" name="lname" placeholder="last name" />
+        <button type="submit">Login</button></form>
     </div>
   );
 }
